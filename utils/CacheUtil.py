cc0b507c4cad64258ae0fb27bb3e0801a4e4f484
suemi94 <suemi994@gmail.com>
2015-12-08 01:08:12 +0800

    1. 增加了语料处理的类 2. 增加了语料处理中一些中间文件的读取的帮助类

--- utils/CacheUtil.py
+++ utils/CacheUtil.py
@@ -3,3 +3,48 @@ Created on 2015年12月5日
 
 @author: suemi
 '''
+from utils.CorpusHandler import CorpusMode
+from gensim import corpora
+class CacheUtil:
+    dataPath="/Volumes/MAC/Rnews/data/"
+    dictionaryPath=CacheUtil.dataPath+"rnews.dict"
+    corpusPath=CacheUtil.dataPath+"corpus.mm"
+        
+    @staticmethod
+    def loadDictionary():
+        return corpora.dictionary.Dictionary.load(CacheUtil.dictionaryPath)
+    
+    @staticmethod
+    def loadCorpus(mode=CorpusMode.Mm):
+        corpus=None
+        CacheUtil.corpusPath=CacheUtil.dataPath+"corpus."+mode
+        if mode==CorpusMode.Mm:
+            corpus=corpora.MmCorpus(CacheUtil.corpusPath)
+        elif mode==CorpusMode.Blei:
+            corpus=corpora.BleiCorpus(CacheUtil.corpusPath)
+        elif mode==CorpusMode.Low:
+            corpus=corpora.LowCorpus(CacheUtil.corpusPath)
+        elif mode==CorpusMode.SVMLight:
+            corpus=corpora.SvmLightCorpus(CacheUtil.corpusPath)
+        return corpus
+    
+    @staticmethod
+    def dumpDictionary(dictionary):
+        dictionary.save(CacheUtil.dictionaryPath)
+    
+    @staticmethod
+    def dumpCorpus(corpus,mode):
+        CacheUtil.corpusPath=CacheUtil.dataPath+"corpus."+mode
+        if mode==CorpusMode.Mm:
+            corpora.MmCorpus.serialize(CacheUtil.corpusPath,corpus)
+        elif mode==CorpusMode.Blei:
+           corpora.BleiCorpus.serialize(CacheUtil.corpusPath,corpus)
+        elif mode==CorpusMode.Low:
+           corpora.LowCorpus.serialize(CacheUtil.corpusPath,corpus)
+        elif mode==CorpusMode.SVMLight:
+            corpora.SvmLightCorpus.serialize(CacheUtil.corpusPath,corpus)
+        
+    
+    @staticmethod
+    def dumpTopicToDB(corpus):
+        pass
---
+++ utils/CorpusHandler.py
@@ -0,0 +1,69 @@
+'''
+Created on 2015年12月5日
+
+@author: suemi
+@summary: 进行词义分析的工具类
+'''
+from model.Entity import *
+from gensim import corpora, models
+from enum import Enum
+from gensim.models import ldamodel
+from utils.CacheUtil import CacheUtil
+class WordProviderFromDB(object):
+    def __iter__(self):
+        for wordBag in WordBag.objects.no_cache():
+            yield wordBag.wordList
+
+class CorpusProvider(object):
+    def __init__(self,dictionary):
+        self.dictioanry=dictionary
+        
+    def __iter__(self):
+        for article in Article.objects.no_cache():
+            yield self.dictionary.doc2bow(article.content)
+            
+class CorpusMode(Enum):
+    Bow="bow"
+    Mm="mm"
+    Blei="lda-c"
+    SVMLight="svmlight"
+    Low="low"
+    
+class TopicMethod(Enum):
+    LDA=0
+
+class CorpusHandler:
+    
+    def __init__(self,wordProvider=None,dictionary=None,corpus=None):
+        self.wordProvider=wordProvider if wordProvider else WordProviderFromDB()
+        self.dictionary=dictionary
+        self.corpus=corpus
+    
+    def generateDictionary(self):
+        dictionary=corpora.Dictionary(self.wordProvider)
+  #      stop_ids = [dictionary.token2id[stopword] for stopword in stoplist  if stopword in dictionary.token2id]  
+        stop_ids=[]
+        once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+        dictionary.filter_tokens(stop_ids + once_ids)
+        dictionary.compactify()
+        self.dictionary=dictionary
+        return self.dictionary
+
+    def generateCorpus(self):
+        if not self.corpus:
+            self.corpus=CorpusProvider(self.dictionary)
+        return self.corpus
+
+    def generateTopic(self,method=TopicMethod.LDA,numTopics=20):
+        if method==TopicMethod.LDA:
+            model=ldamodel.LdaModel(self.corpus,id2word=self.dictionary,num_topics=numTopics)
+            dstCorpus=model[self.corpus]
+        return dstCorpus
+    
+    def process(self,method=TopicMethod.LDA,numTopics=20):
+        if not self.dictionary:
+            self.generateDictionary()
+        if not self.corpus:
+            self.generateCorpus()
+        corpus=self.generateTopic(method, numTopics)
+        CacheUtil.dumpTopicToDB(corpus)
